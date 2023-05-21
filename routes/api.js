d85544600868d0e27b7ac1c8483d55743930f6df
rondeo-balos <47505842+rondeo-balos@users.noreply.github.com>
2023-05-21 07:04:52 +0000

    one test left

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,65 @@
 'use strict';
+const chaiHttp = require('chai-http');
+const chai = require('chai');
+const assert = chai.assert;
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
 
-module.exports = function (app) {
+const stockSchema = new Schema({
+  stock: String, // String is shorthand for {type: String}
+  ip: String,
+  date: { type: Date, default: Date.now }
+});
+
+const StockDB = mongoose.model( 'Stock', stockSchema );
+
+chai.use(chaiHttp);
+
+module.exports = function(app) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
+    .get(function(req, res) {
+      var data = {
+        stockData: {
+          stock: "",
+          price: 0,
+          likes: 0
+        }
+      };
+      var stock = req.query['stock'];
+      var isArray = Array.isArray( stock );
+      var like = req.query[ 'like' ];
+      if( isArray ) {
+        data.stockData = [];
+        var length = stock.length;
+        stock.forEach( function( e, i ) {
+          chai.request('https://stock-price-checker-proxy.freecodecamp.rocks')
+            .get('/v1/stock/' + e + '/quote')
+            .then(responses => {
+              data.stockData.push( {} );
+              var responseJson = JSON.parse( responses.text );
+              data.stockData[i].stock = responseJson.symbol;
+              data.stockData[i].price = responseJson.latestPrice;
+              data.stockData[i].rel_likes = 0;
+              console.log( data.stockData[i] );
+              if( i >= length - 1 ) {
+                res.send( data );
+              }
+            });
+        } );
+        //res.send(data);
+      } else {
+        chai.request('https://stock-price-checker-proxy.freecodecamp.rocks')
+        .get('/v1/stock/' + stock + '/quote')
+        .then(responses => {
+          var responseJson = JSON.parse( responses.text );
+          data.stockData.stock = responseJson.symbol;
+          data.stockData.price = responseJson.latestPrice;
+          data.stockData.likes = 0;
+          res.send(data);
+        });
+      }
       
     });
-    
+
 };
--- server.js
+++ server.js
@@ -1,25 +1,38 @@
 'use strict';
 require('dotenv').config();
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const cors        = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const apiRoutes         = require('./routes/api.js');
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const runner            = require('./test-runner');
+const apiRoutes = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner');
 
 const app = express();
 
+const helmet = require('helmet');
+
+app.use(helmet.noCache());
+app.use( helmet({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'"],
+      styleSrc: ["'self'"]
+    }
+  }
+}) );
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(cors({ origin: '*' })); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Index page (static HTML)
 app.route('/')
-  .get(function (req, res) {
+  .get(function(req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
   });
 
@@ -27,8 +40,8 @@ app.route('/')
 fccTestingRoutes(app);
 
 //Routing for API 
-apiRoutes(app);  
-    
+apiRoutes(app);
+
 //404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
@@ -37,14 +50,14 @@ app.use(function(req, res, next) {
 });
 
 //Start our server and tests!
-const listener = app.listen(process.env.PORT || 3000, function () {
+const listener = app.listen(process.env.PORT || 3000, function() {
   console.log('Your app is listening on port ' + listener.address().port);
-  if(process.env.NODE_ENV==='test') {
+  if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
-    setTimeout(function () {
+    setTimeout(function() {
       try {
         runner.run();
-      } catch(e) {
+      } catch (e) {
         console.log('Tests are not valid:');
         console.error(e);
       }
